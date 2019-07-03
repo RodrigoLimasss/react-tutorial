a5568033ebf43bcf16238adc7f14e4cfce953ad0
Rodrigo Lima <rodrigo.lima@addcode.io>
2019-07-03 22:25:43 +0100

    Using two loops to make the squares

--- src/index.js
+++ src/index.js
@@ -14,28 +14,27 @@ function Square(props) {
 class Board extends React.Component {
     renderSquare(i) {
         return <Square 
+        key={i}
         value={this.props.squares[i]} 
         onClick={() => this.props.onClick(i)}/>;
+    }
+
+    createRow = () => {
+        let rows = [];
+        for(let i = 0; i < 3; ++i){
+            let squares = [];
+            for(let j = 0; j < 3; ++j){
+                squares.push(this.renderSquare(i*3+j))
+            }
+            rows.push(<div key={i} className="board-row">{squares}</div>);
+        }
+        return rows;
     }
 
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {this.createRow()}               
             </div>
         );
     }
